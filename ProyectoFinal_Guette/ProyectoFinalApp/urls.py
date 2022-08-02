fe08d30f5bdf2ad3e8727cfe3738239672886256
Guette Gabriel Alejandro <106787223+guettegabriel@users.noreply.github.com>
2022-08-01 21:46:42 -0300

    Edicion 50%

--- ProyectoFinal_Guette/ProyectoFinalApp/urls.py
+++ ProyectoFinal_Guette/ProyectoFinalApp/urls.py
@@ -26,8 +26,8 @@ urlpatterns = [
     path('Sega/list', SegaList.as_view(), name="Sega_list"),
     path(r'^(?P<pk>\d+)$', SegaDetail.as_view(), name="Sega_detail"),
     path(r'^nuevo$', SegaCreate.as_view(), name="Sega_create"),
-    path(r'^editar/(?P<pk>\d+)$', SegaUpdate.as_view(), name="Sega_update"),
-    path(r'^eliminar/(?P<pk>\d+)$', SegaDelete.as_view(), name="Sega_delete"),
+    path(r'^editar/Sega/(?P<pk>\d+)$', SegaUpdate.as_view(), name="Sega_update"),
+    path(r'^eliminar/Sega/(?P<pk>\d+)$', SegaDelete.as_view(), name="Sega_delete"),
 
 
     
@@ -40,14 +40,20 @@ urlpatterns = [
     path(r'GameBoy/list', GameBoyList.as_view(), name="GameBoy_list"),
     path(r'^(?P<pk>\d+)$', GameBoyDetail.as_view(), name="GameBoy_detail"),
     path(r'^nuevo$', GameBoyCreate.as_view(), name="GameBoy_create"),
-    path(r'^editar/(?P<pk>\d+)$', GameBoyUpdate.as_view(), name="GameBoy_update"),
-    path(r'^eliminar/(?P<pk>\d+)$', GameBoyDelete.as_view(), name="GameBoy_delete"),
+    path(r'^editar/GameBoy(?P<pk>\d+)$', GameBoyUpdate.as_view(), name="GameBoy_update"),
+    path(r'^eliminar/GameBoy(?P<pk>\d+)$', GameBoyDelete.as_view(), name="GameBoy_delete"),
 
     path('Nes/', Ness, name="Nes"),
     path(r'Nes/list', NesList.as_view(), name="Nes_list"),
     path('crear_Nes/', crear_Nes, name="crear_Nes"),
     path('eliminar_Nes/<Nes_id>/', eliminar_Nes, name="eliminar_Nes"),
     path('editar_Nes/<Nes_id>/', editar_Nes, name="editar_Nes"),
+    
+    path(r'Nes/list', NesList.as_view(), name="Nes_list"),
+    path(r'^(?P<pk>\d+)$', NesDetail.as_view(), name="Nes_detail"),
+    path(r'^nuevo$', NesCreate.as_view(), name="Nes_create"),
+    path(r'^editar/Nes(?P<pk>\d+)$', NesUpdate.as_view(), name="Nes_update"),
+    path(r'^eliminar/Nes(?P<pk>\d+)$', NesDelete.as_view(), name="Nes_delete"),
    
     
     
